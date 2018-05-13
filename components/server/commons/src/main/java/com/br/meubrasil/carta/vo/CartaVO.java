678de8b9aa9f6d510b7d44ec44585ff1446fba06
Jorge Demetrio Sobieski Teixeira <jorgedemetrio@alldreams.com.br>
2018-05-13 02:44:30 -0300

    Hibernate

---
+++ components/server/commons/src/main/java/com/br/meubrasil/carta/vo/CartaVO.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.br.meubrasil.carta.vo;
+
+import java.io.Serializable;
+
+/**
+ * @author Jorge Demetrio
+ *
+ */
+public class CartaVO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5740005715312815492L;
+
+}
---
+++ components/server/service/src/main/java/com/br/meubrasil/carta/domain/Carta.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.br.meubrasil.carta.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author Jorge Demetrio
+ *
+ */
+public class Carta implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -35456546351385435L;
+
+}
